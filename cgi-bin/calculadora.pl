123ea59a7148cb806ac0b0dcceeec039107bd0b3
leonhatches <jhatches@unsa.edu.pe>
2024-10-29 21:57:07 -0500

    Version de cgi de calculadora fallida

--- cgi-bin/calculadora.pl
+++ cgi-bin/calculadora.pl
@@ -18,3 +18,55 @@ if ($expresion =~ m/(.\d)+(.\d)/)
 
 print $cgi->header('text/html');
 print<<BLOCK;
+<!DOCTYPE html>
+<html>
+	<head>
+		<!--Extensión para caracteres especiales-->
+		<meta charset="utf-8">
+
+		<!--fuente de letra-->
+		<link
+			href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+			rel="stylesheet"
+			type="text/css">
+
+		<!--CSS-->
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+		<title>Calculadora</title>
+	</head>
+
+	<body>
+		<div class="cajaAzul">
+			<h1>< CALCULADORA ></h1>
+		</div>
+
+		<div class="cajaNormal">
+			<h2>Ingrese su expresión matemática:</h2>
+			
+			<table>
+				<tr>
+					<th>SUMA</th><th>RESTA</th><th>MULTIPLICACIÓN</th><th>DIVISIÓN</th>
+				</tr>
+				<tr>
+					<td>+</td><td>-</td><td>*</td><td>/</td>
+				</tr>
+			</table>
+
+			<h1 style="color: #11213d;">▼</h1>
+		</div>
+
+		<div class="cajaFormu">
+			<form action='cgi-bin/calculadora.pl' method='GET'>
+			<input type='text' name='expresion' class="cajaTexto">
+			<input type='submit' value='CALCULAR' class="botonCalcular">
+		</div>
+
+		<div class="cajaResul">
+			<h2>Resultado:</h2>
+			<p class="resultado">$resultado</p>
+		</div>
+		
+	</body>
+</html>
+BLOCK
